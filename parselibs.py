5a72e7487e461a2aaf08e5cf93cf31f652be39f9
gcrossland <none@none>
2013-10-31 22:24:45 +0000

    Updated parselibs to reflect change in library naming conventions

--- parselibs.py
+++ parselibs.py
@@ -4,19 +4,17 @@ import sys
 
 
 def main (args):
-  syntax = "Syntax: parselibs.py <action> [<lib name> <version>] ..."
+  syntax = "Syntax: parselibs.py <action> [<libname-maj.min>] ..."
   if len(args) < 1:
     sys.exit(syntax)
   action = args[0]
-  args = args[1:]
-  if len(args) % 2 != 0:
-    sys.exit(syntax)
   dependees = []
-  i = 0
-  while i != len(args):
-    libName = args[i]
-    versionStr = args[i + 1]
-
+  for s in args[1:]:
+    dash = s.find('-')
+    if dash == -1:
+      sys.exit(syntax)
+    name = s[:dash]
+    versionStr = s[dash + 1:]
     dot = versionStr.find('.')
     if dot == -1:
       sys.exit(syntax)
@@ -27,21 +25,15 @@ def main (args):
       versionMin = int(versionMinStr)
     except:
       sys.exit(syntax)
-
-    dependees.append((libName, versionMaj, versionMin))
-    i += 2
-
+    dependees.append((name, versionMaj, versionMin))
   actionFn = eval("action_" + action)
   actionFn(dependees)
 
+def action_libnames (list):
+  print " ".join((name for name, maj, min in list))
+
 def action_dependenciesdefn (list):
   print "static const bool DEPENDSON = (" + "".join(("dependson(" + name + "," + str(maj) + "," + str(min) + ")," for name, maj, min in list)) + "true);"
-
-def action_libdirs (list):
-  print " ".join((name.lower() + "-" + str(maj) + "." + str(min) for name, maj, min in list))
-
-def action_libnames (list):
-  print " ".join((name.lower() for name, maj, min in list))
 
 
 
