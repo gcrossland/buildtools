9090cfbb19da798d23d263d205566a78bd4bbada
gcrossland <none@none>
2014-07-25 22:01:27 +0100

    Updated dependency checking scheme

--- parselibs.py
+++ parselibs.py
@@ -1,5 +1,8 @@
 # -*- coding: iso-8859-1 -*-
 import sys
+import os
+import os.path
+import glob
 
 
 
@@ -29,11 +32,32 @@ def main (args):
   actionFn = eval("action_" + action)
   actionFn(dependees)
 
+def action_availablelibs (list):
+  libCacheDir = os.environ.get('LIBCACHEDIR', None)
+  if not libCacheDir:
+    sys.exit("LIBCACHEDIR is not available")
+
+  for i in xrange(0, len(list)):
+    (name, maj, min) = list[i]
+    t = glob.glob(os.path.join(libCacheDir, name + "-" + str(maj) + ".*"))
+    if len(t) != 1:
+      sys.exit("There are " + {True: "no", False: "multiple"}[len(t) == 0] + " builds for " + name + "-" + str(maj) + ".x")
+    leafName = os.path.split(t[0])[1]
+    try:
+      foundMin = int(leafName[leafName.rfind('.') + 1:])
+    except:
+      sys.exit(leafName + " is not a valid build directory name")
+    if foundMin < min:
+      sys.exit(leafName + " is available, but is too old a version")
+    list[i] = (name, maj, foundMin)
+
+  print " ".join((name + "-" + str(maj) + "." + str(min) for name, maj, min in list))
+
 def action_libnames (list):
   print " ".join((name for name, maj, min in list))
 
 def action_dependenciesdefn (list):
-  print "static const bool DEPENDSON = (" + "".join(("dependson(" + name + "," + str(maj) + "," + str(min) + ")," for name, maj, min in list)) + "true);"
+  print "static const bool DEPENDSON = (" + "".join(("_dependson_(" + name + "," + str(maj) + "," + str(min) + ")," for name, maj, min in list)) + "true);"
 
 
 
